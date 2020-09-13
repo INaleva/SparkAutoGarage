993e47cd274f215a4ddb08c2322d911d08452182
inaleva <ilianaleva@gmail.com>
2020-09-13 10:54:09 +0300

    Added add/remove service to car.

--- SparkAuto/Pages/Services/Create.cshtml.cs
+++ SparkAuto/Pages/Services/Create.cshtml.cs
@@ -54,5 +54,29 @@ namespace SparkAuto.Pages.Services
             return Page();
 
         }
+
+        public async Task<IActionResult> OnPostAddToCart()
+        {
+            ServiceShoppingCart objServiceCart = new ServiceShoppingCart()
+            {
+                CarId = CarServiceVM.Car.Id,
+                ServiceId = CarServiceVM.ServiceDetails.ServiceTypeId
+            };
+
+
+            _db.ServiceShoppingCart.Add(objServiceCart);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Create", new { carId = CarServiceVM.Car.Id });
+        }
+
+        public async Task<IActionResult> OnPostRemoveFromCart(int serviceTypeId)
+        {
+            ServiceShoppingCart objServiceShoppingCart = _db.ServiceShoppingCart
+                .FirstOrDefault(u => u.CarId == CarServiceVM.Car.Id && u.ServiceId == serviceTypeId);
+
+            _db.ServiceShoppingCart.Remove(objServiceShoppingCart);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Create", new { CarId = CarServiceVM.Car.Id });
+        }
     }
 }
